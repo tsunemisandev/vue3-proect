4c01ea94d26971fc5088af24105be96a59e5926d
tsunemisan <kombutarou@gmail.com>
2024-03-11 22:54:06 +0900

    Include element plus

---
+++ my-vue-app/src/components/Text.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        Hi
+    </div>
+</template>
+
+<script>
+export default {
+    setup () {
+        
+
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
